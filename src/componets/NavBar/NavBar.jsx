86d898deb7690a6b51cbcc3ed8a82cb910070f9c
Castro Agustin <cagustinivan@gmail.com>
2023-08-17 01:45:07 -0300

    Castro Agustin 1ra Pre Entrega

---
+++ src/componets/NavBar/NavBar.jsx
@@ -0,0 +1,21 @@
+import CartWidget from "../CartWidget/CartWidget";
+
+const NabVar = () => {
+  return (
+    <nav>
+      <h3> Vapes Swag </h3>
+
+      <div>
+        <button>Producto</button>
+        <button>Producto</button>
+        <button>Producto</button>
+        <button>Producto</button>
+
+      </div>
+
+      <CartWidget />
+    </nav>
+  );
+};
+
+export default NabVar;
